286f275f1eb94fd1bef04c95a3cd91151cc29f71
lionelrishi <rshah@bugbare.co.uk>
2018-07-02 20:23:48 +0000

    Completed Successful login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # Handle a successful update.
+      flash[:success] = "Profle Updated"
+      redirect_to @user
     else
       render 'edit'
     end
