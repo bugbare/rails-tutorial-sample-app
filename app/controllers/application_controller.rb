6414f0943cafe36a1c4c70416fc50411d112c0c2
lionelrishi <rshah@bugbare.co.uk>
2018-11-06 22:33:59 +0000

    Added microposts controller and tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,16 @@ class ApplicationController < ActionController::Base
       redirect_to root_url
     end
   end
+  
+  private
+  
+    # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  
 end
